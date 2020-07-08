f87ae0ef31045853be4dc67286f95705669f7937
Corey Carter <crc2231@gmail.com>
2020-07-08 09:25:20 -0600

    Create shopping cart class and test

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/product'
+require './lib/shopping_cart'
+
+class ShoppingCartTest < Minitest::Test
+
+  def test_it_exists
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_instance_of ShoppingCart, cart
+  end
+
+  def test_it_has_a_name
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "King Soopers", cart.name
+  end
+
+  def test_it_has_a_capacity
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal "30items", cart.capacity
+  end
+
+  def test_initial_products_empty
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal [], cart.products
+  end
+
+  
+
+end
